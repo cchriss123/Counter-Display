b4d1af874c2700b0638b4ef04e7bf5d73ebf51fd
Christofer Nyström <christofer.nystrom@iths.se>
2024-08-09 19:44:16 +0200

    updates logo and text slightly

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,21 +1,24 @@
 import './style.css';
-import './app.css';
 
-import logo from './assets/images/logo-universal.png';
+import logo from './assets/images/logo.png';
 import { EventsOn } from '../wailsjs/runtime';
 
 document.querySelector('#app').innerHTML = `
     <img id="logo" class="logo">
     <div class="result" id="result">Waiting for data to display...</div>
-    <div class="input-box" id="input">
+    <div class="result" id="input">
     </div>
 `;
 document.getElementById('logo').src = logo;
 
 let resultElement = document.getElementById("result");
 
-
+// Listen for data from the backend
 EventsOn("dataFromBackend", function(data) {
-    resultElement.innerText = `Received data: ${data}`;
-    console.log(data)
+    let parsedData = JSON.parse(data);
+
+    let formattedData = JSON.stringify(parsedData, null, 4);
+
+    resultElement.innerHTML = `<pre>${formattedData}</pre>`;
+    console.log(parsedData);
 });
