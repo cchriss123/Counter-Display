9da500c2e3b2573ff8a3118c96ae9ebd45df3706
Christofer Nyström <christofer.nystrom@iths.se>
2024-08-10 18:55:58 +0200

    data now shows again

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,51 +1,55 @@
 import './style.css';
-
-import logo from './assets/images/logo.png';
 import { EventsOn } from '../wailsjs/runtime';
 
+// Initial HTML setup
 document.querySelector('#app').innerHTML = `
-
-<div class="outerContainer">
-
-    <div class="desktop">
-        <div class="leftContainer">
-            <div class="containerRed"></div>
-            <div class="containerBlue"></div>
-        </div>
-        <div class="containerGreen"></div>
-    </div>
-    
-
-    <div class="mobile">
-        <div class="containerRed"></div>
-        <div class="container">
-            <div class="containerBlue"></div>
+    <div class="outerContainer">
+        <div class="desktop">
+            <div class="leftContainer">
+                <div class="containerRed">
+                    <div class="result">Waiting for data to display...</div>
+                    <div class="input"></div>
+                </div>
+                <div class="containerBlue"></div>
+            </div>
             <div class="containerGreen"></div>
         </div>
+        
+        <div class="mobile">
+            <div class="containerRed">
+                <div class="result">Waiting for data to display...</div>
+                <div class="input"></div>
+            </div>
+            <div class="container">
+                <div class="containerBlue"></div>
+                <div class="containerGreen"></div>
+            </div>
+        </div>
     </div>
-    
-    
-</div>
 `;
-document.getElementById('logo').src = logo;
-
-let resultElement = document.getElementById("result");
 
 // Listen for data from the backend
 EventsOn("dataFromBackend", function(data) {
     let parsedData = JSON.parse(data);
     let name = parsedData.name;
-
-
     let formattedData = JSON.stringify(parsedData, null, 4);
 
+    // Update all result elements inside containerRed
+    document.querySelectorAll('.containerRed .result').forEach(resultElement => {
+        resultElement.innerHTML = `
+            <div>
+                <p>Hello ${name}!</p>
+                <pre>${formattedData}</pre>
+            </div>
+        `;
+    });
 
-    resultElement.innerHTML = `
-        <div>
-            <p>Hello ${name}!</p>
-            <pre>
-                    ${formattedData}
-            </pre>
-        </div>`;
+    // Update all input elements inside containerRed
+    // document.querySelectorAll('.containerRed .input').forEach(inputElement => {
+    //     inputElement.innerHTML = `
+    //         <div>Additional input or data can be placed here.</div>
+    //     `;
+    // });
+
     console.log(parsedData);
 });
