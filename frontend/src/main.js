91d41b7402e83dc5fb7fb9a9a708fbae9456154d
Christofer Nyström <christofer.nystrom@iths.se>
2024-08-22 14:51:56 +0200

    addscolours to different lines on info containers

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,5 @@
 import './style.css';
-import { EventsOn, EventsEmit } from '../wailsjs/runtime';
+import {EventsEmit, EventsOn} from '../wailsjs/runtime';
 
 
 document.querySelector('#app').innerHTML = `
@@ -94,54 +94,97 @@ EventsOn("dataFromBackend", function(data) {
         `;
     });
 
-    updateContainerBlue();
-    window.addEventListener('resize', updateContainerBlue);
+    updateZoneInfo();
+    window.addEventListener('resize', updateZoneInfo);
 
-    function updateContainerBlue() {
+    function updateZoneInfo() {
         document.querySelectorAll('.zoneInfoContainer').forEach(resultElement => {
             const isWideScreen = window.innerWidth > 1300;
-            resultElement.innerHTML = `
-            <div><strong>Zone Info</strong></div>
-            <div></div>
-            ${isWideScreen ? '<br><div></div>' : ''}
-            <div>Grafts count: </div>
-            <div>${grafts}</div>
-            <div>Hairs count:</div>
-            <div>${hairs}</div>
-            <div>Hair per graft:</div>
-            <div>${hairPerCountedGraft.toFixed(2)}</div>
-            <div>Area:</div>
-            <div>${area}</div>
-            ${isWideScreen ? '' : '<br><div></div>'}
-            <div>Target: </div>
-            <div>${graftsExtractedToReachDonorDesiredCoverageValue}</div>
-            <div>Left: </div>
-            <div>${graftsLeftToReachDonorDesiredCoverageValue}</div>
-            <br>
-        `;
+            resultElement.innerHTML = isWideScreen
+                ?
+                `
+                <div class="white"><strong>Zone Info</strong></div>
+                <div class="white"></div>
+                <div></div>
+                <div><br></div>
+                <div class="blueLeft">Grafts count: </div>
+                <div class="blueRight">${grafts}</div>
+                <div class="white">Hairs count:</div>
+                <div class="white">${hairs}</div>
+                <div class="white">Hair per graft:</div>
+                <div class="white">${hairPerCountedGraft.toFixed(2)}</div>
+                <div class="blueLeft">Area:</div>
+                <div class="blueRight">${area}</div>  
+                <div class="blueLeft">Target: </div>
+                <div class="blueRight">${graftsExtractedToReachDonorDesiredCoverageValue}</div>
+                <div class="white">Left: </div>
+                <div class="white">${graftsLeftToReachDonorDesiredCoverageValue}</div>
+         
+                `
+                :
+                `<div class="white"><strong>Zone Info</strong></div>
+                <div class="white"></div>
+                <div class="blueLeft">Grafts count: </div>
+                <div class="blueRight">${grafts}</div>
+                <div class="white">Hairs count:</div>
+                <div class="white">${hairs}</div>
+                <div class="blueLeft">Hair per graft:</div>
+                <div class="blueRight">${hairPerCountedGraft.toFixed(2)}</div>
+                <div class="white">Area:</div>
+                <div class="white">${area}</div>
+                <div class="blueLeft"><br></div>
+                <div class="blueRight"></div>       
+                <div class="white">Target: </div>
+                <div class="white">${graftsExtractedToReachDonorDesiredCoverageValue}</div>
+                <div class="blueLeft">Left: </div>
+                <div class="blueRight">${graftsLeftToReachDonorDesiredCoverageValue}</div>
+                <br>
+            `;
         });
     }
 
+
+/*
+    <div class="white"><strong>Zone Info</strong></div>
+    <div class="white"></div>
+    <div class="blueLeft">Grafts count: </div>
+    <div class="blueRight">${grafts}</div>
+    <div class="white">Hairs count:</div>
+    <div class="white">${hairs}</div>
+    <div class="blueLeft">Hair per graft:</div>
+    <div class="blueRight">${hairPerCountedGraft.toFixed(2)}</div>
+    <div class="white">Area:</div>
+    <div class="white">${area}</div>
+    <div>Target: </div>
+    <div>${graftsExtractedToReachDonorDesiredCoverageValue}</div>
+    <div>Left: </div>
+    <div>${graftsLeftToReachDonorDesiredCoverageValue}</div>
+    <br>
+    */
+
+
     document.querySelectorAll('.overallInfoContainer').forEach(resultElement => {
         resultElement.innerHTML = `
-            <div><strong>Overall Info</strong></div>
-            <div></div>
-            <div>Total Singles: </div>
-            <div>${totalSingles}</div>
-            <div>Total Doubles: </div>
-            <div>${totalDoubles}</div>
-            <div>Total Triples: </div>
-            <div>${totalTriples}</div>
-            <div>Total Quads: </div>
-            <div>${totalQuadruples}</div>
-            <br>
-            <div></div>
-            <div>Total Grafts: </div>
-            <div>${totalGrafts}</div>
-            <div>Total Hair: </div>
-            <div>${totalHair}</div>
-            <div>Total Hair per Graft: </div>
-            <div>${totalHairPerGraftsCounted.toFixed(2)}</div>
+            <div class="white"><strong>Overall Info</strong></div>
+            <div class="white"></div>
+            <div class="blueLeft">Total singles: </div>
+            <div class="blueRight">${totalSingles}</div>
+            <div class="white">Total doubles: </div>
+            <div class="white">${totalDoubles}</div>
+            <div class="blueLeft">Total triples: </div>
+            <div class="blueRight">${totalTriples}</div>
+            <div class="white">Total quads: </div>
+            <div class="white">${totalQuadruples}</div>
+            
+          
+            <div class="blueLeft"><br></div>
+            <div class="blueRight"></div>
+            <div class="white">Total grafts: </div>
+            <div class="white">${totalGrafts}</div>
+            <div class="blueLeft">Total hair: </div>
+            <div class="blueRight">${totalHair}</div>
+            <div class="white">Hair per graft: </div>
+            <div class="white">${totalHairPerGraftsCounted.toFixed(2)}</div>
 
         `;
     });
@@ -149,9 +192,9 @@ EventsOn("dataFromBackend", function(data) {
 
     function updateTimer() {
         document.querySelector('.topContainer').innerHTML = `
-            <div class="innerTop">Active zone ${activeName}</div> 
-            <div class="innerTop">Time elapsed ${formatTime(timer)}</div>
-            <div class="innerTop">Time of day ${new Date().toLocaleTimeString()}</div> 
+            <div class="innerTop">Active zone: ${activeName}</div> 
+            <div class="innerTop">Time elapsed: ${formatTime(timer)}</div>
+            <div class="innerTop">Time of day: ${new Date().toLocaleTimeString()}</div> 
     `;
     }
 
